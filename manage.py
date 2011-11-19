59d43ed051f8a252d44253aa459964d0b555b608
Costa Halicea <costa@halicea.com>
2011-11-19 18:48:37 +0200

    custom search for armans added to the page

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ try:
     from halicea import hal
     print "Hal web is not located in your python path"
 except:
-    print "accessing halweb thru path {{hal_path}}"
-    sys.path.append("{{hal_path}}")
+    print "accessing halweb thru path /home/costa/DevApps/halicea"
+    sys.path.append("/home/costa/DevApps/halicea")
     import hal
 if __name__=='__main__':
     hal.main_safe(sys.argv)
